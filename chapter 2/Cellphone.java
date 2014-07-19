f062b32e4de1088e394051560f8c41e0953e9421
Boxing Peng <bxpeng@thoughtworks.com>
2014-07-19 22:15:35 +0800

    C2: Add two example of class. Boxing

---
+++ chapter 2/Cellphone.java
@@ -0,0 +1,26 @@
+public class Cellphone {
+    private String type;
+    private double length;
+    private double width;
+    private double height;
+    private String color;
+    private double price;
+
+    public Cellphone(String type, double length, double width, double height, String color, double price){
+        this.type=type;
+        this.length=length;
+        this.width=width;
+        this.height=height;
+        this.color=color;
+        this.price=price;
+    }
+
+    public void call(){
+    }
+    public void takePhoto(){
+    }
+    public void sendMessage(){
+    }
+    public void receiveMessage(){
+    }
+}
---
+++ chapter 2/Television.java
@@ -0,0 +1,17 @@
+public class Television{
+    String shape;
+    double length;
+    double width;
+    double height;
+    String color;
+    int voltage;
+    public void powerSwith(){
+    }
+    public void setChannel(){
+    }
+    public void setVolume(){
+    }
+    public void chooseInput(){
+    }
+}
+    

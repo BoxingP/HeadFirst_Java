0263f4f71dbbd44e8c81d4dc6e001638341d96b6
Boxing Peng <bxpeng@thoughtworks.com>
2014-08-08 14:48:20 +0800

    H1: Delete the DefineSpecialNumber class. Boxing

--- homework/FizzBuzzWhizz/src/main/FizzBuzzWhizz.java
+++ homework/FizzBuzzWhizz/src/main/FizzBuzzWhizz.java
@@ -1,12 +1,18 @@
 public class FizzBuzzWhizz {
 
     public static void main(String[] args){
-        int[] specialNumber = new DefineSpecialNumber().defineSpecialNumber();
+        InputAndOutput inputAndOutput = new InputAndOutput();
+        int[] specialNumber;
         
+        System.out.println("Please input special numbers which should be three different digits:");
+        String inputString = inputAndOutput.getInput();
+        specialNumber = new Converter(inputString).convert();
+        if (!new Checker(specialNumber).check()) { return; }
+
         System.out.println("Count off!");
         for (int testNumber=1;testNumber<101;testNumber++) { 
             String result = new Changer(specialNumber).change(testNumber);
-            new InputAndOutput().printOutput(result);
+            inputAndOutput.printOutput(result);
         }
     }
 }

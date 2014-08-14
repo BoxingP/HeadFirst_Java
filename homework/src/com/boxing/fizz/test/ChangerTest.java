d2752aead30072444836d0567ceb7f0e63248252
Boxing Peng <bxpeng@thoughtworks.com>
2014-08-14 13:54:59 +0800

    H1: change the implement of ChangerTest. Boxing

--- homework/src/com/boxing/fizz/test/ChangerTest.java
+++ homework/src/com/boxing/fizz/test/ChangerTest.java
@@ -2,95 +2,57 @@ package com.boxing.fizz;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+@RunWith(Parameterized.class)
 public class ChangerTest {
-
-    @Test
-    public void numberContainsTheFirstShouldReturnFizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(12), is("Fizz"));
-    }
-
-    @Test
-    public void numberIsMultipleOfTheFirstShouldReturnFizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(4), is("Fizz"));
-    }
-
-    @Test
-    public void numberIsMultipleOfTheSecondShouldReturnBuzz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(9), is("Buzz"));
-    }
-
-    @Test
-    public void numberIsMultipleOfTheThirdShouldReturnWhizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(5), is("Whizz"));
-    }
+    private Changer changer;
+    private int testNumber;
+    private String multipleTag;
     
-    @Test
-    public void numberIsMultipleOfTheFourthShouldReturnMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(49), is("Murmur"));
+    @Before
+    public void initObject() {
+        changer = new Changer(new int[] {2,3,5,7});
     }
-    
-    @Test
-    public void numberIsMultipleOfTheFirstAndSecondShouldReturnFizzBuzz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(6), is("FizzBuzz"));
+
+    @Parameters
+    public static Collection createValue() {
+        return Arrays.asList(new Object[][] {
+                {12, "Fizz"},
+                {4, "Fizz"},
+                {9, "Buzz"},
+                {5, "Whizz"},
+                {49, "Murmur"},
+                {6, "FizzBuzz"},
+                {10, "FizzWhizz"},
+                {14, "FizzMurmur"},
+                {15, "BuzzWhizz"},
+                {63, "BuzzMurmur"},
+                {35, "WhizzMurmur"},
+                {105, "BuzzWhizzMurmur"},
+                {70, "FizzWhizzMurmur"},
+                {84, "FizzBuzzMurmur"},
+                {30, "FizzBuzzWhizz"},
+                {630, "FizzBuzzWhizzMurmur"},
+                {11, "11"}});
+    }
+
+    public ChangerTest(int testNumber, String multipleTag) {
+        this.testNumber = testNumber;
+        this.multipleTag = multipleTag; 
     }
 
     @Test
-    public void numberIsMultipleOfTheFirstAndThirdShouldReturnFizzWhizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(10), is("FizzWhizz"));
-    }
-    
-    @Test
-    public void numberIsMultipleOfTheFirstAndFourthShouldReturnFizzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(14), is("FizzMurmur"));
-    }
-
-    @Test
-    public void numberIsMultipleOfTheSecondAndThirdShouldReturnBuzzWhizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(15), is("BuzzWhizz"));
-    }
-
-    @Test
-    public void numberIsMultipleOfTheSecondAndFourthShouldReturnBuzzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(63), is("BuzzMurmur"));
-    }
-    
-    @Test
-    public void numberIsMultipleOfTheThirdAndFourthShouldReturnWhizzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(35), is("WhizzMurmur"));
-    }
-    
-    @Test
-    public void numberIsMultipleOfAllExceptFirstShouldReturnBuzzWhizzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(105), is("BuzzWhizzMurmur"));
-    }
-
-    @Test
-    public void numberIsMultipleOfAllExceptSecondShouldReturnFizzWhizzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(70), is("FizzWhizzMurmur"));
-    }
-
-    @Test
-    public void numberIsMultipleOfAllExceptThirdShouldReturnFizzBuzzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(84), is("FizzBuzzMurmur"));
-    }
-
-    @Test
-    public void numberIsMultipleOfAllExceptFourthShouldReturnFizzBuzzWhizz() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(30), is("FizzBuzzWhizz"));
-    }
-
-    @Test
-    public void numberIsMultipleOfAllShouldReturnFizzBuzzWhizzMurmur() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(630), is("FizzBuzzWhizzMurmur"));
-    }   
-
-    @Test
-    public void numberNotRelateToAnyShouldReturnItself() {
-        assertThat(new Changer(new int[] {2,3,5,7}).change(11), is("11"));
+    public void testNumberDifferentConditionsShouldReturnCorrectly() {
+        assertThat(changer.change(testNumber), is(multipleTag));
     }
 
 }

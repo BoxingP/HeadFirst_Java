1a165d597263c862dbb658b9d0ea3b5d931a1381
Boxing Peng <bxpeng@thoughtworks.com>
2014-08-24 23:43:11 +0800

    H1: fix the bug of importing jar. Boxing

--- homework/src/test/java/com/boxing/unit/ConverterTest.java
+++ homework/src/test/java/com/boxing/unit/ConverterTest.java
@@ -4,8 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
     

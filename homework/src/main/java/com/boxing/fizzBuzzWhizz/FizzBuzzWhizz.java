2deff135af73a4542237f714711f395b941ab737
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-18 07:22:38 +0800

    H1: delete rulelist which are not been used in FizzBuzzWhizz. Boxing

--- homework/src/main/java/com/boxing/fizzBuzzWhizz/FizzBuzzWhizz.java
+++ homework/src/main/java/com/boxing/fizzBuzzWhizz/FizzBuzzWhizz.java
@@ -5,8 +5,6 @@ import com.boxing.unit.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.List;
-
 public class FizzBuzzWhizz {
     private static ApplicationContext context;
 
@@ -25,24 +23,10 @@ public class FizzBuzzWhizz {
             throw new IllegalArgumentException("Only non repeating digits are allowed!");
         }
 
-        Rule chain = createChain(initRuleList());
+        Rule chainEntrance = (Rule) context.getBean("containRule");
         for (int number = 1; number < 101; number++) {
-            String result = chain.replace(number, specialNumbers);
+            String result = chainEntrance.replace(number, specialNumbers);
             inputAndOutput.printOutput(result);
         }
-    }
-
-    private static List<Rule> initRuleList() {
-
-        List<Rule> ruleList = (List<Rule>) context.getBean("ruleList");
-        return ruleList;
-    }
-
-    private static Rule createChain(List<Rule> ruleList) {
-        ruleList.size();
-        for (int index = 0; index < ruleList.size() - 1; index++) {
-            ruleList.get(index).setNext(ruleList.get(index + 1));
-        }
-        return ruleList.get(0);
     }
 }

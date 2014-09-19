37ac54ee24deae234154abb4d9d0ad4e0a5457ce
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-19 13:51:40 +0800

    H1: extract methods in MultipleRule. Boxing

--- homework/src/main/java/com/boxing/rule/MultipleRule.java
+++ homework/src/main/java/com/boxing/rule/MultipleRule.java
@@ -9,13 +9,17 @@ public class MultipleRule extends Rule {
 
     public String replace(int number, int[] specialNumbers) {
         StringBuilder sequence = new StringBuilder();
-
-        for (int index = 0; index < specialNumbers.length; index++) {
-            if (number % specialNumbers[index] == 0) {
-                sequence.append(multipleSpecialString[index]);
-            }
-        }
-        return sequence.length() != 0 ? sequence.toString() : doNext(number, specialNumbers);
+        return isMultiple(number, specialNumbers, sequence) ? sequence.toString() : doNext(number, specialNumbers);
     }
 
+    private boolean isMultiple(int number, int[] specialNumbers, StringBuilder sequence) {
+        for (int index = 0; index < specialNumbers.length; index++) {
+            replaceMultipleToString(number, specialNumbers, sequence, index);
+        }
+        return sequence.length() != 0;
+    }
+
+    private StringBuilder replaceMultipleToString(int number, int[] specialNumbers, StringBuilder sequence, int index) {
+        return number % specialNumbers[index] == 0 ? sequence.append(multipleSpecialString[index]) : sequence;
+    }
 }
